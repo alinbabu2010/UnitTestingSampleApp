d82e6fc210e1e331d7f031e204483befabeefb04
Alin Babu <alinbabu2010@gmail.com>
2022-11-17 15:13:06 +0530

    Add test class for TestFunctionUtils

---
+++ app/src/test/java/com/sample/unittest/TestFunctionUtilsTest.kt
@@ -0,0 +1,45 @@
+package com.sample.unittest
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+
+class TestFunctionUtilsTest {
+
+    @Test
+    fun `fib returns 3 for n=5`() {
+        val result = TestFunctionUtils.fib(5)
+        assertThat(result).isEqualTo(3)
+    }
+
+    @Test
+    fun `fib returns 0 for n=0`() {
+        val result = TestFunctionUtils.fib(0)
+        assertThat(result).isEqualTo(0)
+    }
+
+    @Test
+    fun `fib returns 1 for n=1`() {
+        val result = TestFunctionUtils.fib(1)
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun `fib not returns 5 for n=5`() {
+        val result = TestFunctionUtils.fib(5)
+        assertThat(result).isNotEqualTo(5)
+    }
+
+    @Test
+    fun `checkBraces returns true for correct input`() {
+        val result = TestFunctionUtils.checkBraces("((a+b)*(a-b))")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `checkBraces returns false for incorrect input`() {
+        val result = TestFunctionUtils.checkBraces("((a+b*(a-b))")
+        assertThat(result).isFalse()
+    }
+
+}
